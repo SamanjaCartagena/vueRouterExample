4f3f8a920aa6bddcf10b512b40310f597c052353
SamanjaCartagena <cart.samanja09@gmail.com>
2022-06-20 17:42:24 -0400

    first commit

---
+++ src/components/pages/CreatePost.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Create Posts
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,28 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import HomePage from './components/pages/HomePage.vue'
+import PostsPage from './components/pages/PostsPage.vue'
+import CreatePost from './components/pages/CreatePost.vue'
+import { createRouter, createWebHistory} from 'vue-router';
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+const routes = [
+    { path: '/', component:HomePage
+    
+    },
+    { path: '/posts', component:PostsPage
+    
+    },
+    { path: '/createpost', component:CreatePost
+    
+    },
+
+]
+const router = createRouter({
+    history: createWebHistory(),
+    routes: routes,
+
+})
+app.use(router)
+app.mount('#app')
